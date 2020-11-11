8d472f82a9a7ab919de9be6b53b58ab675876618
mehdizit <zitounimehdi.it@gmail.com>
2020-11-11 16:41:50 +0100

    process dup_update

--- fork.c
+++ fork.c
@@ -6,10 +6,29 @@
 
 int main()
 {
+	pid_t id;
 	system("ps -f");
-	printf("\n\n\n\n");
-	fork();                     /*duplication_system_process*/
-	system("ps -f");
+	id = fork();                     /*duplication_system_process*/
+/*	system("ps -f");*/
+/*	printf(" v = %i\n\n\n\n", v);*/
 
+	switch( id )
+	{
+	case -1:
+		printf("ERROR !");
+		break;
+	case 0:
+		printf("FILS\n");
+		system("ps -f");
+		printf("\n");
+		break;
+	default:
+		wait(NULL); /* killed the parent before the current complite her execution*/
+		printf("PERE\n");
+		system("ps -f");
+		printf("\n");
+		break;
+	}
+		
 	return(0);
 }
