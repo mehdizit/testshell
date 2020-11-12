afe8409f5ef9b2d6375c62ac86e638f9b117331d
mehdizit <zitounimehdi.it@gmail.com>
2020-11-12 21:31:16 +0100

    prompt

---
+++ prompt.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - prints "$ ", wait for the user to enter a command,
+ * prints it on the next line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *buff = NULL;
+	size_t len = 0;
+
+	printf("$ ");
+	while (getline(&buff, &len, stdin) != -1)
+	{
+		printf("%s", buff);
+		printf("$ ");
+	}
+	free(buff);
+	return (0);
+}
