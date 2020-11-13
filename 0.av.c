505ee51cd40f51af02a6e92e1a9235fab100faa4
mehdizit <zitounimehdi.it@gmail.com>
2020-11-13 15:26:02 +0100

    print all arguments witout using ac argument

--- 0.av.c
+++ 0.av.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
-
+#include "shell.h"
+/**
+ *main - main function that print all arguments without using ac param
+ *@ac: integer
+ *@av: string
+ *Return: int
+ */
 int main(int __attribute__((unused)) ac, char **av)
 {
-	printf("%s\n", *av);
+	int count;
+
+	if (ac > 0)
+	{
+		for (count = 0; count < ac; count++)
+		{
+			printf("%s\n", av[count]);
+		}
+	}
 	return (0);
 }
