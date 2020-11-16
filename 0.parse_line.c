6a1ac8395f182ddaff796b57a2aba22f5d1fe332
mehdizit <zitounimehdi.it@gmail.com>
2020-11-16 16:49:21 +0100

    update

--- 0.parse_line.c
+++ 0.parse_line.c
@@ -1,16 +1,19 @@
+#include "shell.h"
+
 int process_line(char* args[], char line[])
 {
         int i = 0;
         args[i] = strtok(line, " ");
         if (args[i] == NULL)
         {
+		printf("NO COMMANDE\n");
                 return (1);
         }
         
         while (args[i] != NULL)
         {
                 i++;
-                args[i] = strtok(line, " ");
+                args[i] = strtok(NULL, " ");
         }
 
         
