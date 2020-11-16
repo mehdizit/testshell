bbee323c47d7c4f9639989e18fbaf19f5ac8b4e9
mehdizit <zitounimehdi.it@gmail.com>
2020-11-16 05:15:35 +0100

    update

--- shell.h
+++ shell.h
@@ -2,14 +2,17 @@
 #define SHELL_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-
-#define BUFSIZE 1024
-#define DELIM " "
-char **_parse(char *str);
-char *_strdup(char *str);
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int read_parse_line(char* args[], char line[]);
+int process_line(char* args[], char line[]);
+void read_line(char line[]);
+void remove_endofline(char line[]);
+#define MAX_WORD 10
+#define MAX_CHAR 100
+#define DEL " "
 
 #endif
 
