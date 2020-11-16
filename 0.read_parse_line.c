d575bc41f024c1b985c0067ce333f6600c5c62a7
mehdizit <zitounimehdi.it@gmail.com>
2020-11-17 00:35:43 +0100

    update

--- 0.read_parse_line.c
+++ 0.read_parse_line.c
@@ -1,10 +1,14 @@
 #include "shell.h"
+/**
+ *read_parse_line - read token of line stream
+ *@args: char
+ *@line: char
+ *Return: 1 if true
+ */
 
-
-int read_parse_line(char* args[], char line[])
+int read_parse_line(char *args[], char line[])
 {
-        read_line(line);
-
-        process_line(args, line);
-        return (1);
+	read_line(line);
+	process_line(args, line);
+	return (1);
 }
