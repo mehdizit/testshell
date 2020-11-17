38c552c0635226176b70c660f82ded7303498b4c
mehdizit <zitounimehdi.it@gmail.com>
2020-11-17 12:37:18 +0100

    display argument

---
+++ execve.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+int _execve(void)
+{
+	char *argv[] = {"/bin/ls", "-l", "/home/mahdi/holbertonschool-low_level_programming", NULL};
+
+ 
+    
+    execve(argv[0], argv, NULL);
+    
+    
+    return (0);
+}
+	
