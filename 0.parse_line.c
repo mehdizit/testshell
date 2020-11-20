09e24372a365c8b0c0a5d6fa1103a848e13a5e9c
mehdizit <zitounimehdi.it@gmail.com>
2020-11-20 15:46:47 +0100

    split read line

--- 0.parse_line.c
+++ 0.parse_line.c
@@ -1,21 +1,40 @@
 #include "shell.h"
 
-int process_line(char* args[], char line[])
+
+
+char **split_line(char *str, const char *delim)
 {
         int i = 0;
-        args[i] = strtok(line, " ");
-        if (args[i] == NULL)
-        {
-		printf("NO COMMANDE\n");
-                return (1);
-        }
-        
-        while (args[i] != NULL)
-        {
-                i++;
-                args[i] = strtok(NULL, " ");
-        }
+	char *str1 = NULL, *str2 = NULL, *ptr = NULL, **ar = NULL;
 
-        
-        return (1);
+	if (((str) == NULL) || ((delim) == NULL))
+		return (NULL);
+	str1 =  _strdup(str);
+	str2 =  _strdup(str);
+
+	if (!(str1) || !(str2))
+		return (NULL);
+	ptr = strtok(str1, delim);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; ptr != NULL; i++)
+	{
+		ptr = strtok(NULL, delim);
+	}
+	ar = malloc(sizeof(char *) * (i + 1));
+	if (ar == NULL)
+		return (NULL);
+	ptr = strtok(str2, delim);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; ptr != NULL; i++)
+	{
+		ar[i] = _strdup(ptr);
+		ptr = strtok(NULL, delim);
+	}
+	printf("%s" "%s", *ar, ptr);
+	ar[i] = NULL;
+	free(str1);
+	free(str2);
+	return (ar);
 }
