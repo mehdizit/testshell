31cc762da6318a33d5337aa19fd8313588902f1e
mehdizit <zitounimehdi.it@gmail.com>
2020-11-21 02:03:44 +0100

    update

--- 0.main.c
+++ 0.main.c
@@ -11,37 +11,51 @@
  *@argv: char
  *Return: Always 0.
  */
-int main(int argc, char **argv, char **env)
+
+int main(int __attribute__((unused)) argc, char **argv, char **env)
 {
-	
-	char *line = NULL;
+	(void) argv;
 	size_t status = 0;
-	char ** tokens;
-	char **array;
-	char **ch = split_line(search_var("PATH", env), ":=");
-	char **ch2 = ch + 1;
-
-
+	int i = 0;
+	char *str; 
+	char **ch;
+	char *line, **tokens;
+	 str = search_var("PATH", env);
+	 ch  = split_string(str,":=");
+/*	char** ch2 = ch + 1;*/
 	while (1)
 	{
+		status = 0;
+		line = NULL;
+		tokens = NULL;
+		i++;
 		printf("$ ");
+	
 		if (getline(&line, &status, stdin) == -1)
 		{
-			write(STDIN_FILENO, "\n", 1);
-			return (1);
+				write(STDIN_FILENO, "\n", 1);
+				return (1);
 			free(line);
 			break;
 		}
 		if (line != NULL)
 		{
-			tokens = split_line(line, "\n");
+			if (line[0] == '\n')
+			{
+				free(line);
+				line = NULL;
+			}
+		}
+		if (line != NULL)
+		{
+			tokens = split_string(line, "\n ");
 			free(line);
 		}
-		if _built((array, ch2))
+			if (_built(tokens, env))
 			break;
-		/*	free_array(array);*/
+			free_array(tokens);
 	}
+	free_array(ch);
+	free_array(tokens);
+	return (0);
 }
-		
-		
-
