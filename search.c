eba54d659e592a7a10bb44a68f11bb399b38d98c
mehdizit <zitounimehdi.it@gmail.com>
2020-11-23 22:20:40 +0100

    update

--- search.c
+++ search.c
@@ -1,24 +1,23 @@
 #include "shell.h"
-
-
-
-
+/**
+ *search_var - search fo path binary to be executed
+ *@str: char
+ *@env: array of string for PATH
+ *Return: NULL.
+ */
 char *search_var(char *str, char **env)
 {
 	int i, j;
 
 	if ((str == NULL) || (env) == NULL)
 		return (NULL);
-	
 	for (i = 0; env[i] != NULL; i++)
 	{
 		j = 0;
-	
 		while (str[j] != '\0' && str[j] == env[i][j])
 			j++;
 		if (str[j] == '\0')
 			return (env[i]);
-		
 	}
 	return (NULL);
 }
