a51d012838e27acc3dd4151f23efe42a61a12b75
mehdizit <zitounimehdi.it@gmail.com>
2020-11-23 21:51:58 +0100

    update

--- built.c
+++ built.c
@@ -1,33 +1,35 @@
 #include "shell.h"
-
-int _built(char **ar, char **argv, char **env, char **p_t, int i)
+/**
+ *_built - fork and execute
+ *@ar: char
+ *@argv: char
+ *@env: char
+ *@s:char
+ *@i:integer
+ *Return: 0 on success
+ */
+int _built(char **ar, char **argv, char **env, char **s, int i)
 {
-	
 	char *ptr = NULL;
 	char *str = NULL;
 
 	if (ar != NULL && !(_strcmp(ar[0], "exit")))
-	{
-		
-			return (1);
-	}
+		return (1);
 	else if (ar != NULL && !(_strcmp(ar[0], "env")))
-	{
-		print_env(env);
-	}
+		_getenv(env);
 	else if (ar != NULL && access(ar[0], F_OK) == -1)
 	{
 		ptr = ar[0];
-		ar[0] = _cat(p_t, ar[0]);
+		ar[0] = _cat_token_path(s, ar[0]);
 		if (ar[0] == NULL)
 		{
 			ar[0] = _strdup("(nil)");
-			str = _itoa(i);
-			ERR_EXE(argv[0], str, ptr);
+			str = _atoi(i);
+			ERROR(argv[0], str, ptr);
 			free(str);
 		}
 		free(ptr);
-		}
+	}
 	if (ar != NULL && _strcmp(ar[0], "(nil)"))
 		execute(ar);
 
