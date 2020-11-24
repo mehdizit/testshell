33720b5412c84bdd3df5e4041e06dcc88feb3c85
mehdizit <60630120+mehdizit@users.noreply.github.com>
2020-11-25 00:51:35 +0100

    up

--- 1_parse_line.c
+++ 1_parse_line.c
@@ -5,35 +5,21 @@
  *@delim: char
  *Return: array of string
  */
-char **split_string(char *str, const char *delim)
+char **split_string(FILE *fp)
 {
-	int i = 0;
-	char *str_1 = NULL, *str_2 = NULL, *ptr = NULL, **ar = NULL; _strdup(str);
+	char *line;
+	ssize_t read;
+	size_t len;
 
-	if (!(str) || !(delim))
-		return (NULL);
-	str_1 = _strdup(str);
-	str_2 = _strdup(str);
-	if (!(str_1) || !(str_2))
-		return (NULL);
-	ptr = strtok(str_1, delim);
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; ptr != NULL; i++)
-		ptr = strtok(NULL, delim);
-	ar = malloc(sizeof(char *) * (i + 1));
-	if (ar == NULL)
-		return (NULL);
-	ptr = strtok(str_2, delim);
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; ptr != NULL; i++)
+	line = NULL;
+	len = 0;
+	read = getline(&line, &len, fp);
+	if (read == -1)
 	{
-		ar[i] = _strdup(ptr);
-		ptr = strtok(NULL, delim);
+		free(line);
+		exit(EXIT_SUCCESS);
 	}
-	ar[i] = NULL;
-	free(str_1);
-	free(str_2);
-	return (ar);
+
+	return (line);
 }
+
