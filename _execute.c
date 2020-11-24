dc7e9885d8d4962dca5c90ad6b96a7bfdb32bf17
mehdizit <60630120+mehdizit@users.noreply.github.com>
2020-11-25 00:55:56 +0100

    up

--- _execute.c
+++ _execute.c
@@ -5,24 +5,50 @@
  *Return: Always 0.
  */
 
-int execute(char **str)
+int builtin_execute(char **tokens)
 {
-	pid_t child_pid;
+	int status;
+	unsigned int length;
+	unsigned int num;
+	unsigned int i;
 
-	child_pid = fork();
-	if (child_pid == -1)
+	built_s builtin[] = {
+		{"exit", shell_exit},
+		{"env", shell_env},
+		{NULL, NULL};
+
+	if (tokens[0] == NULL)
 		return (1);
 
-	if (child_pid == 0)
-	{
-		if (execve(str[0], str, NULL) == -1)
-			exit(1);
-	}
-	else
-	{
-		wait(NULL);
-		return (0);
-	}
+	length = _strlen(tokens[0]);
 
-	return (0);
+	num = shell_num_builtins(builtin);
+	for (i = 0; i < num; i++)
+	{
+		if (_strcmp(tokens[0], builtin[i].name, length) == 0)
+		{
+			status = (builtin[i].p)();
+			return (status);
+		}
+	}
+	return (1);
+}
+
+/**
+**shell_num_builtins - this check num built-ins
+**@builtin: takes the builtin to be counted
+**Return: num of built-ins
+**/
+
+int shell_num_builtins(built_s builtin[])
+{
+	unsigned int i;
+
+	i = 0;
+	while (builtin[i].name != NULL)
+		i++;
+
+	return (i);
+}
+
 }
