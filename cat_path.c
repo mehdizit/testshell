8f638bff1cdfe332410feddfdcb20ba3442ceaa5
mehdizit <60630120+mehdizit@users.noreply.github.com>
2020-11-25 00:59:52 +0100

    up

--- cat_path.c
+++ cat_path.c
@@ -1,50 +1,27 @@
-#include "shell.h"
-/**
- *_cat_token_path - split the string PATH into tokens
- *@path: char
- *@cmd: char
- *Return: NULL
- */
-
-char *_cat_token_path(char **path, char *cmd)
+char **tokenizer(char *str)
 {
-	char *buff = NULL;
-	int i = 0, j = 0, k = 0;
+	char **tokens;
+	char *token;
+	unsigned int i;
 
-	if (!(path) || !(cmd))
-		return (NULL);
-	if (cmd[0] == '/')
-		return (NULL);
-	while (path[k] != NULL)
+	tokens = malloc(sizeof(char) * BUFFER);
+	if (tokens == NULL)
 	{
-		buff = malloc(sizeof(char) * (_strlen(path[k]) + _strlen(cmd) + 2));
-		if (!(buff))
-			return (NULL);
-		i = 0, j = 0;
-		while (path[k][j])
-		{
-			buff[i] = path[k][j];
-			i++;
-			j++;
-		}
-		buff[i] = '/';
-		i++;
-		j = 0;
-		while (cmd[j])
-		{
-			buff[i] = cmd[j];
-			i++;
-			j++;
-		}
-		buff[i] = '\0';
-
-		if (access(buff, F_OK) == 0)
-		{
-			return (buff);
-		}
-		else
-			free(buff);
-		k++;
+		errors(3);
+		exit(EXIT_FAILURE);
 	}
-	return (NULL);
+
+	token = strtok(str, "\n\t\r ");
+
+	i = 0;
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		token = strtok(NULL, "\n\t\r ");
+		i++;
+	}
+
+	tokens[i] = NULL;
+
+	return (tokens);
 }
