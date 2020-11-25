186832c74d1a7841c83346bd419b26b2378b0acc
mehdizit <60630120+mehdizit@users.noreply.github.com>
2020-11-25 01:01:25 +0100

    Update env.c

--- env.c
+++ env.c
@@ -1,19 +1,43 @@
-#include "shell.h"
-/**
- *_getenv - print environnment
- *@name: char
- *Return: Always 0.
- */
-
-int _getenv(char **name)
+char *_getenv(const char *name)
 {
-	int i = 0;
+	char **environ_copy;
+	char *variable, *value, *path;
+	int compare;
+	unsigned int path_length, environ_length, length, i;
 
-	while (name[i])
+	environ_length = 0;
+	while (environ[environ_length] != NULL)
+		environ_length++;
+	environ_copy = NULL;
+	environ_copy = copy_env(environ_copy, environ_length);
+
+	length = _strlen((char *)name);
+	i = 0;
+	while (environ_copy[i] != NULL)
 	{
-		write(STDOUT_FILENO, name[i], _strlen(name[i]));
-		write(STDOUT_FILENO, "\n", 1);
+		variable = environ_copy[i];
+		compare = _strncmp((char *)name, variable, length);
+		if (compare == 1)
+		{
+			value = strtok(variable, "=");
+			value = strtok(NULL, "\n ");
+			if (value == '\0')
+			{
+				errors(4);
+				exit(EXIT_FAILURE);
+			}
+			path_length = _strlen(value);
+			path = malloc(sizeof(char) * path_length + 1);
+			if (path == NULL)
+			{
+				errors(3);
+				return (NULL);
+			}
+			path = _strcpy(path, value);
+			free_dp(environ_copy, environ_length);
+			return (path);
+		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
